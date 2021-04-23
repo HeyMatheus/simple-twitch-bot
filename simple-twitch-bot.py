7e07b87496884bfd94a49e4759a0753b4a5e0359
HeyMatheus <miliandroxd@gmail.com>
2021-04-23 13:44:45 -0300

    Fix: PING:PONG

--- simple-twitch-bot.py
+++ simple-twitch-bot.py
@@ -1,9 +1,9 @@
 import socket
 import time
 
-CHANNEL = "Gaules"  # Canal que o bot vai escutar
+CHANNEL = "GMHikaru"  # Canal que o bot vai escutar
 NICK = "MatheusKek"  # Usuário da twitch
-OAUTH = "***********"  # https://twitchapps.com/tmi
+OAUTH = "*********"  # https://twitchapps.com/tmi
 
 print(f"-> Conectando ao chat da twitch ({CHANNEL}).")
 sock = socket.socket()
@@ -17,7 +17,7 @@ try:
   while True:
     message = sock.recv(2048).decode("UTF-8")
     if "PING :tmi.twitch.tv" in message:
-      sock.send(bytes("PONG :tmi.twitch.tv\r\n", "UTF-8"))
+      sock.send(bytes("PONG tmi.twitch.tv\r\n", "UTF-8"))
     name = message.split("!", 1)[0][1:]
     message = message.split("PRIVMSG", 1)[-1].split(":", 1)[-1][:-1]
     print(f"{name}: {message}")

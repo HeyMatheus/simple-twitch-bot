c07cdefc2d77335b7b851019a50c9810a57d24ff
HeyMatheus <miliandroxd@gmail.com>
2021-04-23 04:53:37 -0300

    o primeiro e provavelmente o único commit nessa bosta

---
+++ simple-twitch-bot.py
@@ -0,0 +1,27 @@
+import socket
+import time
+
+CHANNEL = "Gaules"  # Canal que o bot vai escutar
+NICK = "MatheusKek"  # Usuário da twitch
+OAUTH = "***********"  # https://twitchapps.com/tmi
+
+print(f"-> Conectando ao chat da twitch ({CHANNEL}).")
+sock = socket.socket()
+time.sleep(1)
+try:
+  print("-> Conectado :D")
+  sock.connect(("irc.chat.twitch.tv", 6667))
+  sock.send(bytes("PASS " + OAUTH + "\r\n", "UTF-8"))
+  sock.send(bytes("NICK " + NICK + "\r\n", "UTF-8"))
+  sock.send(bytes("JOIN #" + CHANNEL + "\r\n", "UTF-8"))
+  while True:
+    message = sock.recv(2048).decode("UTF-8")
+    if "PING :tmi.twitch.tv" in message:
+      sock.send(bytes("PONG :tmi.twitch.tv\r\n", "UTF-8"))
+    name = message.split("!", 1)[0][1:]
+    message = message.split("PRIVMSG", 1)[-1].split(":", 1)[-1][:-1]
+    print(f"{name}: {message}")
+except KeyboardInterrupt:
+  pass
+except:
+  print("-> Deu ruim D:")
